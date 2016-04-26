40ff03ec48b8172dd0f2da5ea7e540862481b528
Faycal Hassani <faycelhassani2@gmail.com>
2016-04-26 19:48:21 -0400

    Add operation log.

--- CcaBank/Program.cs
+++ CcaBank/Program.cs
@@ -36,7 +36,7 @@ namespace CcaBank
                     Menu();
 
                     // run operations
-                    exit = RunOperations(tabAccounts, currentAccount, exit);
+                    exit = RunOperations(tabAccounts, currentAccount, tabOperations, ref nextOperation, exit);
                 }
             }
 
@@ -44,62 +44,17 @@ namespace CcaBank
             Console.ReadKey();
         }
 
-        static bool RunOperations(Account[] tabAccounts, int currentAccount, bool exit)
+        static int InitializeAccounts(Account[] tabAccounts)
         {
-            string tempOperation = Console.ReadLine();
-            switch (tempOperation)
-            {
-                // exit
-                case "0":
-                    exit = true;
-                    break;
-
-                case "exit":
-                    exit = true;
-                    break;
-
-                // retrait
-                case "1":
-                    exit = Retrait(tabAccounts, currentAccount, exit);
-                    break;
-
-                // depot
-                case "2":
-                    exit = Depot(tabAccounts, currentAccount, exit);
-                    break;
-
-                // solde
-                case "3":
-                    Console.WriteLine("Votres solde : " + tabAccounts[currentAccount].Balance);
-                    break;
-
-                // autre
-                default:
-                    Console.WriteLine("Operation invalide");
-                    break;
-            }
-            return exit;
+            tabAccounts[0] = new Account { Number = 100, Pin = 1234, Balance = 2000, ClientName = "George Cloony" };
+            tabAccounts[1] = new Account { Number = 200, Pin = 2345, Balance = 3000, ClientName = "Jon Machin" };
+            tabAccounts[2] = new Account { Number = 300, Pin = 3456, Balance = 100, ClientName = "Forest Moi" };
+            return 3;
         }
 
-        static bool Authenticate(Account[] tabAccounts, int currentAccount)
+        static int InitializeOperations(Operation[] tabOperations)
         {
-            while (true)
-            {
-                Console.Write("Votre Code PIN : ");
-                string tempPin = Console.ReadLine();
-                if (tempPin == "0" || tempPin == "exit")
-                {
-                    return false;
-                }
-
-                int currentPin = Convert.ToInt32(tempPin);
-                if (tabAccounts[currentAccount].Pin != currentPin)
-                {
-                    Console.WriteLine("Code PIN Invalide");
-                    continue;
-                }
-                return true;
-            }
+            return 0;
         }
 
         static int GetAccount(Account[] tabAccounts, int nextAccount)
@@ -133,46 +88,75 @@ namespace CcaBank
             }
         }
 
-        static int InitializeAccounts(Account[] tabAccounts)
-        {
-            tabAccounts[0] = new Account { Number = 100, Pin = 1234, Balance = 2000, ClientName = "George Cloony" };
-            tabAccounts[1] = new Account { Number = 200, Pin = 2345, Balance = 3000, ClientName = "Jon Machin" };
-            tabAccounts[2] = new Account { Number = 300, Pin = 3456, Balance = 100, ClientName = "Forest Moi" };
-            return 3;
-        }
-
-        static int InitializeOperations(Operation[] tabOperations)
-        {
-            return 0;
-        }
-
-        static bool Depot(Account[] tabAccounts, int currentAccount, bool exit)
+        static bool Authenticate(Account[] tabAccounts, int currentAccount)
         {
             while (true)
             {
-                Console.Write("Montant a depositer : ");
-                string tempAmount = Console.ReadLine();
-                if (tempAmount == "0" || tempAmount == "exit")
+                Console.Write("Votre Code PIN : ");
+                string tempPin = Console.ReadLine();
+                if (tempPin == "0" || tempPin == "exit")
                 {
-                    exit = true;
-                    break;
+                    return false;
                 }
 
-                int amount = Convert.ToInt32(tempAmount);
-                if (amount <= 0)
+                int currentPin = Convert.ToInt32(tempPin);
+                if (tabAccounts[currentAccount].Pin != currentPin)
                 {
-                    Console.WriteLine("Le montant a deposer doit etre superieur a 0");
+                    Console.WriteLine("Code PIN Invalide");
                     continue;
                 }
+                return true;
+            }
+        }
 
-                tabAccounts[currentAccount].Balance += amount;
-                Console.WriteLine("Depot complete");
-                break;
+        static void Menu()
+        {
+            Console.WriteLine("Operations disponible :");
+            Console.WriteLine("0- Sortie");
+            Console.WriteLine("1- Retrait");
+            Console.WriteLine("2- Depot");
+            Console.WriteLine("3- Solde de compte");
+            Console.Write("Veuillez saisir le numero de l'operation a effectuer: ");
+        }
+
+        static bool RunOperations(Account[] tabAccounts, int currentAccount, Operation[] tabOperations, ref int nextOperation, bool exit)
+        {
+            string tempOperation = Console.ReadLine();
+            switch (tempOperation)
+            {
+                // exit
+                case "0":
+                    exit = true;
+                    break;
+
+                case "exit":
+                    exit = true;
+                    break;
+
+                // retrait
+                case "1":
+                    exit = Retrait(tabAccounts, currentAccount, tabOperations, ref nextOperation, exit);
+                    break;
+
+                // depot
+                case "2":
+                    exit = Depot(tabAccounts, currentAccount, tabOperations, ref nextOperation, exit);
+                    break;
+
+                // solde
+                case "3":
+                    Console.WriteLine("Votres solde : " + tabAccounts[currentAccount].Balance);
+                    break;
+
+                // autre
+                default:
+                    Console.WriteLine("Operation invalide");
+                    break;
             }
             return exit;
         }
 
-        static bool Retrait(Account[] tabAccounts, int currentAccount, bool exit)
+        static bool Retrait(Account[] tabAccounts, int currentAccount, Operation[] tabOperations, ref int nextOperation, bool exit)
         {
             while (true)
             {
@@ -196,20 +180,46 @@ namespace CcaBank
                     continue;
                 }
                 tabAccounts[currentAccount].Balance -= amount;
+                LogOperation(tabOperations, tabAccounts[currentAccount].Number, -amount, ref nextOperation);
                 Console.WriteLine("Retrait complete");
+                Console.WriteLine("Nouveau solde : " + tabAccounts[currentAccount].Balance);
                 break;
             }
             return exit;
         }
 
-        static void Menu()
+        static bool Depot(Account[] tabAccounts, int currentAccount, Operation[] tabOperations, ref int nextOperation, bool exit)
         {
-            Console.WriteLine("Operations disponible :");
-            Console.WriteLine("0- Sortie");
-            Console.WriteLine("1- Retrait");
-            Console.WriteLine("2- Depot");
-            Console.WriteLine("3- Solde de compte");
-            Console.Write("Veuillez saisir le numero de l'operation a effectuer: ");
+            while (true)
+            {
+                Console.Write("Montant a depositer : ");
+                string tempAmount = Console.ReadLine();
+                if (tempAmount == "0" || tempAmount == "exit")
+                {
+                    exit = true;
+                    break;
+                }
+
+                int amount = Convert.ToInt32(tempAmount);
+                if (amount <= 0)
+                {
+                    Console.WriteLine("Le montant a deposer doit etre superieur a 0");
+                    continue;
+                }
+
+                tabAccounts[currentAccount].Balance += amount;
+                LogOperation(tabOperations, tabAccounts[currentAccount].Number, amount, ref nextOperation);
+                Console.WriteLine("Depot complete");
+                Console.WriteLine("Nouveau solde : " + tabAccounts[currentAccount].Balance);
+                break;
+            }
+            return exit;
+        }
+
+        static void LogOperation(Operation[] tabOperations, int accountNumber, int amount, ref int nextOperation)
+        {
+            tabOperations[nextOperation] = new Operation { AccountNumer = accountNumber, Number = nextOperation, Amount = amount, Date = DateTime.Now };
+            nextOperation++;
         }
     }
 
